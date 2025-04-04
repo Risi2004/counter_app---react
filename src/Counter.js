5df4cb309bc5c93190d0911ea327873af9f05052
Risi2004 <risikesanjegatheesan@gmail.com>
2025-04-04 17:14:48 +0530

    Add files via upload

---
+++ src/Counter.js
@@ -0,0 +1,30 @@
+import { useState } from "react"
+function Counter()
+{
+    var [count,setCount] = useState(0)
+
+    function increment()
+    {
+        var newCount = count + 1
+        setCount(newCount)
+    }
+
+
+    function decrement()
+    {
+        var newCount = count - 1
+        setCount(newCount)
+    }
+
+    return(
+        <div>
+            <h1>Counter</h1>
+            <h1>{count}</h1>
+            <button onClick={increment}>Increment</button>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+
+export default Counter
